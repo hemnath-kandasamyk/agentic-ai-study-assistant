7dbf783c294d900e64cec14b4f3d94b97cd0331a
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-07-04 22:38:52 +0530

    Add logging configuration for study agent

---
+++ logging_config.py
@@ -0,0 +1,25 @@
+import logging
+
+from config import LOG_DIR
+
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("study_agent")
+    logger.setLevel(logging.INFO)
+
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
+    )
+
+    file_handler = logging.FileHandler(LOG_DIR / "agent.log", encoding="utf-8")
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    return logger
